2026a4359846ca33c0ed91359812cfb093108354
Akshay <akshayjee6@gmail.com>
2023-07-29 23:05:00 +0530

    fixed the filter issue

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const postsContainer = document.getElementById("posts-container");
 const loading = document.querySelector(".loader");
+const filter = document.getElementById("filter");
 
 let limit = 5;
 let page = 1;
